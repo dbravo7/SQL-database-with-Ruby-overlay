5b05adf838575e1d952007f41e4ad132ef917bf1
David <dbravo7@yahoo.com>
2019-05-15 15:18:12 -0400

    Populated db and debugged

--- import_db.sql
+++ import_db.sql
@@ -1,12 +1,25 @@
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS users;
+
+PRAGMA foreign_keys = ON;
+
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL,
-  lname TEST NOT NULL,
+  lname TEST NOT NULL
 );
 
-DROP TABLE IF EXISTS questions;
+  INSERT INTO 
+    users (fname, lname)
+  VALUES  
+    ("David", "Bane"), ("Richard", "Lane"), ("Draven", "Marquez");
+
+
+-- QUESTIONS
 
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
@@ -17,7 +30,34 @@ CREATE TABLE questions (
   FOREIGN KEY (student_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS question_follows;
+INSERT INTO 
+  questions (title, body, student_id)
+SELECT 
+  "David's question", "ORTYUETPOUY?", users.id
+FROM 
+  users 
+WHERE 
+  users.fname = "David" AND users.lname = "Bane";
+
+INSERT INTO 
+  questions (title, body, student_id)
+SELECT
+  "Richard's question", "ALFJDALKJLFJ?", users.id
+FROM 
+  users
+WHERE 
+  users.fname = "Richard" AND users.lname = "Lane";
+
+INSERT INTO 
+  questions (title, body, student_id)
+SELECT 
+  "Draven's question", ";LJJOIJLKLKANFLKAF?", users.id
+FROM 
+  users
+WHERE 
+  users.fname = "Draven" AND users.lname = "Marquez";
+
+-- QUESTION_FOLLOWS
 
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
@@ -28,11 +68,24 @@ CREATE TABLE question_follows (
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
-DROP TABLE IF EXISTS replies;
+INSERT INTO 
+  question_follows (user_id, question_id)
+VALUES
+  ((SELECT id FROM users WHERE fname = "David" AND lname = "Bane"),
+    (SELECT id FROM questions WHERE title = "David's question")
+);
+
+INSERT INTO 
+  question_follows (user_id, question_id)
+VALUES  
+  ((SELECT id FROM users WHERE fname = "Richard" AND lname = "Lane"),
+    (SELECT id FROM questions WHERE title = "Richard's question")
+);
+
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
-  parent_reply_id INTEGER NOT KEY,
+  parent_reply_id INTEGER,
   question_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   body TEXT NOT NULL,
@@ -42,14 +95,39 @@ CREATE TABLE replies (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS question_likes;
+  INSERT INTO   
+    replies (parent_reply_id, question_id, user_id, body)
+  VALUES  
+    (NULL,
+    (SELECT id FROM questions WHERE title = "David's question"),
+    
+    (SELECT id FROM users WHERE fname = "David" AND lname = "Bane"),
+    "Please restate the question"
+  );
+
+  INSERT INTO
+    replies (parent_reply_id, question_id, user_id, body)
+  VALUES 
+    ((SELECT id FROM replies WHERE body = "Please restate the question"),
+      (SELECT id FROM questions WHERE title = "David's question"),
+      (SELECT id FROM users WHERE fname = "David" AND lname = "Bane"),
+      "This answer is the answer"
+  );
+
+-- QUESTION_LIKES 
 
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
-  like_question TEXT NOT NULL,
   user_id INTEGER NOT NULL,
-  question_id INTEGER NOT NULL
+  question_id INTEGER NOT NULL,
 
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO 
+  question_likes (user_id, question_id)
+VALUES 
+    ((SELECT id FROM users WHERE fname = "David" AND lname = "Bane"),
+    (SELECT id FROM questions WHERE title = "David's question")
 );
